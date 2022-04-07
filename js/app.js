be3d980b8087e6b7224ea4fcd86cf17322fe4e6f
Dean William Weiss <dean.w.weiss@gmail.com>
2022-04-07 13:41:49 -0700

    Add functions to all questions

--- js/app.js
+++ js/app.js
@@ -8,6 +8,16 @@ let correctCounter = 0;
 
 alert(`Welcome, ${siteVisitor}! Let's play a game! Please answer "Yes" or "No".`);
 
+// functions invoked below
+
+q1();
+q2();
+q3();
+q4();
+q5();
+q6();
+q7();
+
 // Question 1
 
 function q1(){
@@ -34,87 +44,102 @@ function q1(){
 
 // Question 2
 
-loopCheck = false;
+function q2(){
 
-while(loopCheck !== true){
-  let responseTwo = prompt('I have two children. "Yes or No"?').toLowerCase();
+  loopCheck = false;
 
-  if(responseTwo === 'yes' || responseTwo === 'y'){
-    // console.log('Sorry! You are wrong.');
-    alert('Sorry! You are wrong!');
-    loopCheck = true;
-  } else if(responseTwo === 'no' || responseTwo === 'n'){
-    // console.log('You are correct!');
-    alert('You are correct!');
-    correctCounter++;
-    loopCheck = true;
-  } else {
-    alert('Invalid entry. Try "Yes", "Y", "No", or "N".');
+  while(loopCheck !== true){
+    let responseTwo = prompt('I have two children. "Yes or No"?').toLowerCase();
+
+    if(responseTwo === 'yes' || responseTwo === 'y'){
+      // console.log('Sorry! You are wrong.');
+      alert('Sorry! You are wrong!');
+      loopCheck = true;
+    } else if(responseTwo === 'no' || responseTwo === 'n'){
+      // console.log('You are correct!');
+      alert('You are correct!');
+      correctCounter++;
+      loopCheck = true;
+    } else {
+      alert('Invalid entry. Try "Yes", "Y", "No", or "N".');
+    }
+
   }
 
 }
 
 // Question 3
 
-loopCheck = false;
+function q3(){
 
-while(loopCheck !== true){
-  let responseThree = prompt('I like books. "Yes or No"?').toLowerCase();
+  loopCheck = false;
 
-  if(responseThree === 'yes' || responseThree === 'y'){
-    // console.log('You are correct!');
-    alert('You are correct!');
-    correctCounter++;
-    loopCheck = true;
-  } else if(responseThree === 'no' || responseThree === 'n'){
-    // console.log('Sorry! You are wrong.');
-    alert('Sorry! You are wrong.');
-    loopCheck = true;
-  } else {
-    alert('Invalid entry. Try "Yes", "Y", "No", or "N".');
+  while(loopCheck !== true){
+    let responseThree = prompt('I like books. "Yes or No"?').toLowerCase();
+
+    if(responseThree === 'yes' || responseThree === 'y'){
+      // console.log('You are correct!');
+      alert('You are correct!');
+      correctCounter++;
+      loopCheck = true;
+    } else if(responseThree === 'no' || responseThree === 'n'){
+      // console.log('Sorry! You are wrong.');
+      alert('Sorry! You are wrong.');
+      loopCheck = true;
+    } else {
+      alert('Invalid entry. Try "Yes", "Y", "No", or "N".');
+    }
+
   }
 
 }
 
 // Question 4
 
-loopCheck = false;
+function q4(){
 
-while(loopCheck !== true){
-  let responseFour = prompt('I have a car. "Yes or No"?').toLowerCase();
+  loopCheck = false;
 
-  if(responseFour === 'yes' || responseFour === 'y'){
-    // console.log('You are correct!');
-    alert('You are correct!');
-    correctCounter++;
-    loopCheck = true;
-  } else if(responseFour === 'no' || responseFour === 'n'){
-    // console.log('Sorry! You are wrong.');
-    alert('Sorry! You are wrong.');
-    loopCheck = true;
-  } else{
-    alert('Invalid entry. Try "Yes", "Y", "No", or "N".');
+  while(loopCheck !== true){
+    let responseFour = prompt('I have a car. "Yes or No"?').toLowerCase();
+
+    if(responseFour === 'yes' || responseFour === 'y'){
+      // console.log('You are correct!');
+      alert('You are correct!');
+      correctCounter++;
+      loopCheck = true;
+    } else if(responseFour === 'no' || responseFour === 'n'){
+      // console.log('Sorry! You are wrong.');
+      alert('Sorry! You are wrong.');
+      loopCheck = true;
+    } else{
+      alert('Invalid entry. Try "Yes", "Y", "No", or "N".');
+    }
   }
-}
 
+}
 // Question 5
 
-loopCheck = false;
+function q5(){
 
-while(loopCheck !== true){
-  let responseFive = prompt('My favorite vegetable is broccoli. "Yes or No"?').toLowerCase();
+  loopCheck = false;
 
-  if(responseFive === 'yes' || responseFive === 'y'){
-    // console.log('Sorry! You are wrong.');
-    alert('Sorry! You are wrong.');
-    loopCheck = true;
-  } else if(responseFive === 'no' || responseFive === 'n'){
-    // console.log('You are correct!');
-    alert('You are correct!');
-    correctCounter++;
-    loopCheck = true;
-  } else {
-    alert('Invalid entry. Try "Yes", "Y", "No", or "N".');
+  while(loopCheck !== true){
+    let responseFive = prompt('My favorite vegetable is broccoli. "Yes or No"?').toLowerCase();
+
+    if(responseFive === 'yes' || responseFive === 'y'){
+      // console.log('Sorry! You are wrong.');
+      alert('Sorry! You are wrong.');
+      loopCheck = true;
+    } else if(responseFive === 'no' || responseFive === 'n'){
+      // console.log('You are correct!');
+      alert('You are correct!');
+      correctCounter++;
+      loopCheck = true;
+    } else {
+      alert('Invalid entry. Try "Yes", "Y", "No", or "N".');
+    }
+
   }
 
 }
@@ -122,63 +147,65 @@ while(loopCheck !== true){
 // Question 6
 // Ben Small and Sam Brindle helped me work on six. I did not know about ParseInt prior.
 
-let correctAnswer = 33;
-let attempt1 = 4;
-let responseSix;
+function q6(){
 
-for(let i = 0; i < attempt1; i++){
-  responseSix = parseInt(prompt('Guess a number'));
-  if(responseSix === correctAnswer){
-    alert('Ding! Ding! Ding! You are a winner!');
-    correctCounter++;
-    break;
-  } else if(responseSix < correctAnswer){
-    alert('Too low! Guess Again');
-  } else if(responseSix > correctAnswer){
-    alert('Too high! Guess again!');
-  } else {
-    alert('Invalid input!');
+  let correctAnswer = 33;
+  let attempt1 = 4;
+  let responseSix;
 
-  }
-}
-
-if(responseSix !== correctAnswer){
-  alert(`Better luck next time! The answer was ${correctAnswer}`);
-}
-
-// Question 7
-// Ben Small and Sam Brindle helped me work on seven with looping through the array.
-
-let correctOptions = ['halo 2', 'starcraft', 'elden ring'];
-let attempt2 = 6;
-let responseSeven;
-let loopCheck2 = false;
-
-for(let i = 0; i < attempt2; i++){
-  responseSeven = prompt('What is one of my top 10 video games of all time?').toLowerCase();
-
-  for(let j = 0; j < correctOptions.length; j++){
-
-    if(correctOptions[j] === responseSeven){
+  for(let i = 0; i < attempt1; i++){
+    responseSix = parseInt(prompt('Guess a number'));
+    if(responseSix === correctAnswer){
       alert('Ding! Ding! Ding! You are a winner!');
       correctCounter++;
-      loopCheck2 = true;
       break;
+    } else if(responseSix < correctAnswer){
+      alert('Too low! Guess Again');
+    } else if(responseSix > correctAnswer){
+      alert('Too high! Guess again!');
+    } else {
+      alert('Invalid input!');
+
     }
   }
 
-  if(loopCheck2){
-    break;
-  } else if(i === attempt2-1){
-    alert(`Nice try, but you are out of guesses! My favorite games are ${correctOptions}`);
-    break;
-  } else{
-    alert(`Sorry, ${responseSeven} isn't one of them!`);
+
+  if(responseSix !== correctAnswer){
+    alert(`Better luck next time! The answer was ${correctAnswer}`);
+  }
+}
+// Question 7
+// Ben Small and Sam Brindle helped me work on seven with looping through the array.
+
+function q7(){
+
+  let correctOptions = ['halo 2', 'starcraft', 'elden ring'];
+  let attempt2 = 6;
+  let responseSeven;
+  let loopCheck2 = false;
+
+  for(let i = 0; i < attempt2; i++){
+    responseSeven = prompt('What is one of my top 10 video games of all time?').toLowerCase();
+
+    for(let j = 0; j < correctOptions.length; j++){
+
+      if(correctOptions[j] === responseSeven){
+        alert('Ding! Ding! Ding! You are a winner!');
+        correctCounter++;
+        loopCheck2 = true;
+        break;
+      }
+    }
+
+    if(loopCheck2){
+      break;
+    } else if(i === attempt2-1){
+      alert(`Nice try, but you are out of guesses! My favorite games are ${correctOptions}`);
+      break;
+    } else{
+      alert(`Sorry, ${responseSeven} isn't one of them!`);
+    }
   }
 }
 
 alert(`Thanks for playing, ${siteVisitor}! You got ${correctCounter} questions correct!`);
-
-// functions invoked below
-
-q1();
